0492915e1cdefbe9b4a94e4ca901029d1e597e38
Tseho <quentin.favrie@gmail.com>
2017-10-31 12:33:29 +0100

    dev: removes async component & react router

--- src/container.js
+++ src/container.js
@@ -2,19 +2,13 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import PropTypes from 'prop-types'
 
-// Private component, see https://github.com/ctrlplusb/react-async-component/issues/20
-import AsyncComponentProvider from 'react-async-component/commonjs/AsyncComponentProvider'
-import createExecContext from './Async/createExecContext'
-
 class ReactReduxAppContainer extends Component {
     render() {
-        let { store, children } = this.props
+        const { store, children } = this.props
         return (
-            <AsyncComponentProvider execContext={createExecContext()}>
-                <Provider store={store}>
-                    {children}
-                </Provider>
-            </AsyncComponentProvider>
+            <Provider store={store}>
+                {children}
+            </Provider>
         )
     }
 }
